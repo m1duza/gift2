e73deb92001ba3bc3aec862bac3d48173b4b6e6d
m1duza <ivan.zubcov228@gmail.com>
2024-12-21 19:17:13 +0300

    Copy all content from master to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gift/', // Замените 'gift' на имя вашего репозитория
   plugins: [react()],
 })
